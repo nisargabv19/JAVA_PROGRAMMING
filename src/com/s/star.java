53536aa34606641c484b0de95a2c1ef33d48e061
nisargabv19 <nisargabv19@gmail.com>
2025-04-17 21:34:09 +0530

    first commit

---
+++ src/com/s/star.java
@@ -0,0 +1,66 @@
+package com.s;
+
+public class star {
+	/*public static void main(String[] args) 
+	{
+		int rows=5;
+		for (int i=1;i<=5 ; i++){
+			for(int j=1;j<=5;j++){
+		System.out.print(" " +" * ");
+			}
+			System.out.println();
+	}
+}
+}*/
+
+	/*{
+	public static void main(String[] args) 
+	{
+		int rows=5;
+		for (int i=1;i<=5 ; i++){
+			for(int j=1;j<=5;j++){
+		System.out.print(" " + i );
+			}
+			System.out.println();
+	}
+}
+}*/
+	/*{
+	public static void main(String[] args) 
+	{
+		int rows=5;
+		for (int i=1;i<=5 ; i++){
+			for(int j=1;j<=5;j++){
+		System.out.print(" " + j);
+			}
+			System.out.println();
+	}
+}
+}*/
+
+	/*{
+	public static void main(String[] args) 
+	{
+		int rows=5;
+		for (int i=1;i<=5 ; i++){
+			for(int j=1;j<=5;j++){
+		System.out.print((char)(j+64)+ " ");
+			}
+			System.out.println();
+	}
+}
+}*/
+
+	public static void main(String[] args) 
+	{
+		int rows=5;
+		for (int i=1;i<=5 ; i++){
+			for(int j=1;j<=5;j++){
+		System.out.print((char)(i+64)+ " ");
+			}
+			System.out.println();
+	}
+}
+
+
+}
